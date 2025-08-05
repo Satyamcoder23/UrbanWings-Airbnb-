52d13e0ef95273a2531f3edf384239ac840bda6e
Satyamcoder23 <satyam@23>
2025-08-06 04:17:09 +0530

    Update port

--- app.js
+++ app.js
@@ -105,7 +105,8 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
+const port = process.env.PORT || 3000 ;
 // Start server
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("🌐 Server is running on port 3000");
 });
