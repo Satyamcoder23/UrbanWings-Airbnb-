df48c74f9ac710ec5e811abe5ffe37d38ed2554f
Satyamcoder23 <satyam@23>
2025-08-08 00:35:26 +0530

    Update user model

--- models/user.js
+++ models/user.js
@@ -12,3 +12,13 @@ const userSchema = new Schema({
 
 userSchema.plugin(passportLocalMongoose); // Adds username and password fields, and handles hashing 
 module.exports = mongoose.model("User", userSchema);
+
+
+const Listing = require("./listing");
+
+userSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Listing.deleteMany({ author: doc._id });
+    console.log(`🧹 Listings by ${doc._id} deleted`);
+  }
+});
